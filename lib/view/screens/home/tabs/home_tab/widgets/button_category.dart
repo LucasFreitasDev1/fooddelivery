51bb56c9aa84b167d2e6873314b49fcb483b1bc9
Eduarda Lima <eududalim@gmail.com>
2022-01-14 20:18:41 -0300

    Alteraçoes no button_category

--- lib/view/screens/home/tabs/home_tab/widgets/button_category.dart
+++ lib/view/screens/home/tabs/home_tab/widgets/button_category.dart
@@ -1,22 +1,46 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class ButtonCategory extends StatelessWidget {
-  const ButtonCategory({Key key, this.imgUrl}) : super(key: key);
+class ButtonCategory extends StatefulWidget {
+  ButtonCategory({Key key, @required this.imgUrl});
 
   final String imgUrl;
 
   @override
+  _ButtonCategoryState createState() => _ButtonCategoryState(imgUrl);
+}
+
+class _ButtonCategoryState extends State<ButtonCategory> {
+  _ButtonCategoryState(this._img);
+
+  String _img;
+
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: (){},
-      splashColor: Theme.of(context).primaryColor,
-      child: Container(
-        child: CachedNetworkImage(imageUrl: imgUrl, imageBuilder: (context, img)=> CircularProgressIndicator(), ),
-        decoration: BoxDecoration(
-            border: Border.all(color: Theme.of(context).primaryColor),
-            borderRadius: BorderRadius.circular(30)),
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).primaryColor),
+          borderRadius: BorderRadius.circular(30)),
+      child: FutureBuilder (
+        initialData: '',
+        future: FirebaseStorage(storageBucket: _img).ref().getDownloadURL(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData)
+          return InkWell(
+          onTap: () {},
+          splashColor: Theme.of(context).primaryColor,
+          child: CachedNetworkImage(
+            imageUrl: snapshot.data?.toString()
+            ,
+            imageBuilder: (context, img) => CircularProgressIndicator(),
+          ),
+        );
+        return CircularProgressIndicator();
+        },
       ),
     );
   }
